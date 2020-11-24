cace5eb7d18a69f63c73b7b9ff167a0ce422cfef
Kofi114 <sebby2k@protonmail.com>
2020-11-24 10:38:37 -0500

    Update circuit.R
    Rewrote the main so that our state space is now a sequence of 20 cities with no repetitions. There is also a uniformly distributed distance symmetrix matrix with diagonal entries 0.

--- src/circuit.R
+++ src/circuit.R
@@ -1,31 +1,19 @@
-library(compare)
-S <-c(1:20) #state space
-n <- 40
+U <-c(1:20) #state space
+n <- 100
 D <- matrix(runif(n=20*20, min=0, max =1), ncol = 20) #uniformly distributed distance matrix
 D[lower.tri(D)] = t(D)[lower.tri(D)] #symmetric matrix
 diag(D) <- rep(0,20)
 
 
-restrictedPaths_list <- list(c(4,5), c(5,4), c(9,13), c(13,9), c(20,1), c(1,20), c(17,10), c(10,17), c(3,8), c(8,3)) #5 forbidden pairs 
+#restrictedPaths_list <- list(c(4,6), c(6,4), c(9,13), c(13,9), c(19,1), c(1,19), c(17,10), c(10,17), c(3,8), c(8,3)) #5 forbidden pairs 
 
-indicatorForbidden <- function(x,y){ #optimize this so you iterate only 5 times.
-  indtest_ <- c(x,y)
-  for(i in 1:10){
-    comp <- identical(indtest_,unlist(restrictedPaths_list[i]))
-    if(comp){
-      return (0)
-    }
-  }
-  return(1)
-}
 
-x <- rep(0,n)
-x[1] <- 1
+S <- list(c(1:20)) #preferably this sequence will be randomly generated and a proper circuit.
 for(i in 1:n){
-  Q <- sample(S,size=1, prob=D[x[i],])
-  if(indicatorForbidden(x[i],Q)==0){
-    x[i+1] = x[i]       
-  } else {
-    x[i+1] = Q        
-  }
+  P <- sample(1:20,size=1, prob=D[x[i],])
+  Q <- sample(1:20,size=1, prob=D[x[i],])
+  index_p = match(P, unlist(S[i]))
+  index_q = match(Q, unlist(S[i]))
+  k <- unlist(S[i])
+  S[i+1] <- list(c(replace(k, c(P,Q), k[c(Q,P)])))
 }
