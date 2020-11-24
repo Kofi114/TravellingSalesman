450eaf6a7e0e985df5199182488e657ecf81867c
Kofi114 <sebby2k@protonmail.com>
2020-11-24 15:04:35 -0500

    Update circuit.R
    The return_time function is now called only if the proposed interchange is accepted and most importantly a while loop is being used in place of a for-loop so we can ensure that we sample n and m circuits without repetitions.

--- src/circuit.R
+++ src/circuit.R
@@ -48,28 +48,31 @@ return_time <- function(L){
 tspMCMC <- function(n,m){
 S <- list(c(1:20)) #preferably this sequence will be randomly generated and a proper circuit.
 rejection_counter <- 0
+i <- 1
 j <- 1
 travel_time <- rep(0, m-n+1)
-for(i in 1:m){
-  if(i >= n){
-    travel_time[j] = return_time(S[i])
-    j <- j + 1
-  }
+while(i <= m){
   P <- sample(1:20,size=1) #basic sampling 
   Q <- sample(1:20,size=1) #likewise for proposed state
   index_p = match(P, unlist(S[i])) #the position of P in the sequence
   index_q = match(Q, unlist(S[i])) #likewise for the second element in the pair
   k <- unlist(S[i])
   if(indicatorPermissible(k,index_p,index_q) == 1){
+    if(i >= n){
+      travel_time[j] = return_time(S[i])
+      j <- j + 1
+    }
     S[i+1] <- list(c(replace(k, c(P,Q), k[c(Q,P)]))) #adds a new sequence to S interchanging P and Q 
+    i <- i + 1
     #return(S[i+1])
   }
   else{
-    S[i+1] = S[i]
     rejection_counter = rejection_counter+1 #see how many time our proposed transition introduced forbidden travel
   }
 }
   answer = mean(travel_time)
+  print(length(travel_time))
+  print(rejection_counter)
   return(answer)
 }
 
@@ -79,4 +82,6 @@ tspMCMC(400,800)
 tspMCMC(800,1600)
 tspMCMC(1600,3200)
 tspMCMC(3200,6400)
+tspMCMC(20000,40000)
+tspMCMC(200000,400000)
 
