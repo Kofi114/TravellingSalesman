ed4fb02003b673d3e30e04abae77856e86b4e89b
Kofi114 <sebby2k@protonmail.com>
2020-11-24 11:38:04 -0500

    Update circuit.R
    Instead of going through a list to check for forbidden travel, made more sense to consult the distance matrix instead. Thus we have a semi-working function that ensure we reject sequences that introduce forbidden travel.

--- src/circuit.R
+++ src/circuit.R
@@ -1,19 +1,56 @@
 U <-c(1:20) #state space
 n <- 100
 D <- matrix(runif(n=20*20, min=0, max =1), ncol = 20) #uniformly distributed distance matrix
+D[4,6] = 0
+D[9,13]= 0
+D[19,1]= 0 #WTF!
+D[17,10]= 0  #WTF!
+D[3,8] = 0 #NO TRAVEL NO DISTANCE
 D[lower.tri(D)] = t(D)[lower.tri(D)] #symmetric matrix
-diag(D) <- rep(0,20)
+diag(D) <- rep(0,20) #diagonals are 0
 
-
-#restrictedPaths_list <- list(c(4,6), c(6,4), c(9,13), c(13,9), c(19,1), c(1,19), c(17,10), c(10,17), c(3,8), c(8,3)) #5 forbidden pairs 
-
+indicatorForbidden <- function(C,p,q){
+  if(p != 1 && p!= 20 && q!= 1 && q!= 20){ #normal interchange
+    for(i in c(-1,1)){
+      if(D[C[p+i],C[q]] == 0 || D[C[q+i],C[p]]==0){
+        return(0)
+      }
+    }
+  }
+  else{#case where interchange occurs at one or both of the endpoints of sequence
+    if(p == 1){
+      if(D[C[20],C[q]] == 0 || D[C[2],C[q]] == 0)
+        return(0)
+    }
+    else if(p == 20){
+      if(D[C[1],C[q]] == 0 || D[C[19],C[q]] == 0)
+        return(0)
+    }
+    else if(q==1){
+      if(D[C[20],C[p]] == 0 || D[C[2],C[p]] == 0)
+        return(0)
+    }
+    else{
+      if(D[C[1],C[p]] == 0 || D[C[19],C[p]] == 0)
+        return(0)
+    }
+  }
+  return(1)
+}
 
 S <- list(c(1:20)) #preferably this sequence will be randomly generated and a proper circuit.
+rejection_counter = 0
 for(i in 1:n){
-  P <- sample(1:20,size=1, prob=D[x[i],])
-  Q <- sample(1:20,size=1, prob=D[x[i],])
-  index_p = match(P, unlist(S[i]))
-  index_q = match(Q, unlist(S[i]))
+  P <- sample(1:20,size=1, prob=D[x[i],]) #sampling from distance matrix for now
+  Q <- sample(1:20,size=1, prob=D[x[i],]) #likewise for proposed state
+  index_p = match(P, unlist(S[i])) #the position of P in the sequence
+  index_q = match(Q, unlist(S[i])) #likewise for the second element in the pair
   k <- unlist(S[i])
-  S[i+1] <- list(c(replace(k, c(P,Q), k[c(Q,P)])))
+  if(indicatorForbidden(k,index_p,index_q) == 1){
+    S[i+1] <- list(c(replace(k, c(P,Q), k[c(Q,P)]))) #adds a new sequence to S interchanging P and Q 
+  }
+  else{
+    S[i+1] = S[i]
+    rejection_counter = rejection_counter+1 #see how many time our proposed transition introduced forbidden travel
+  }
 }
